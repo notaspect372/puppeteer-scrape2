8ceb46bd689a5d3b026cf8ec161ea8cc6c594be0
notaspect372 <notaspect372@gmail.com>
2024-12-03 13:38:14 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
 const xlsx = require('xlsx');
